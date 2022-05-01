a88360260192636cae0b2668ddebb137feaf20e4
Soroborus <soro.borus@mail.ru>
2022-05-01 16:58:29 +0300

    Добавьте файлы проекта.

---
+++ dz8sem2/Student.cpp
@@ -0,0 +1,18 @@
+#include "Student.h"
+
+
+
+std::istream& operator >> (std::istream& in, Student& a) {
+	setlocale(LC_ALL, "rus");
+	std::cout << "Input your student's  surname, name, course and group:" << std::endl; //перегруж. оператор ввода
+	in >> a.surname >> a.name >> a.course >> a.group;
+	return in;
+}
+std::ostream& operator <<(std::ostream& os, Student& a) {
+	os << a.surname << " " << a.name << " course: " << a.course << "" << " group: " << a.group << std::endl;  //перегруж. оператор вывода
+	return os;
+}
+
+
+
+
